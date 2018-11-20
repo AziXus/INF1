2a89cc5f33162ea2d4c3540a720e69feb7599272
Naludrag <stephanecarvalho81@gmail.com>
2018-11-20 21:19:52 +0100

    Update annexe.h

--- Labo_08/annexe.h
+++ Labo_08/annexe.h
@@ -1,18 +1,13 @@
 /*
  -----------------------------------------------------------------------------------
  Laboratoire : Labo_08_Clavier
- Fichier     : main.cpp
+ Fichier     : annexe.h
  Auteur(s)   : Robin Müller, Stéphane Teixeira Carvalho
  Date        : 21.11.2018
 
- But         : Générer des questions pour tester les réflexes de l'utilisateur.
-               L'utilisateur choisi le nombre de lances entre 1 et 10.
-               Un résultat indiquant le nombre de réponses correcteset le temps effectué
-               est donné en fin de programme
+ But         : gérer les focntion d'affichages et entrée sortie du programme
 
- Remarque(s) : Utilisation de la fonction ctime pour calculer le temps mis pour résoudre le questionnaire.
-               Le nombre de lances doit être comrpis entre 1 et 10.
-               Utilisation des libraires annexe et reflex.              
+ Remarque(s) : Utilisation de la fonction srand pour générer des nombres aléatoires             
 
  Compilateur : MinGW-g++ 6.3.0
  -----------------------------------------------------------------------------------
