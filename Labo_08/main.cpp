7fdbd80c328669b2138832f1e6e0696b6e4b7c9b
Naludrag <stephanecarvalho81@gmail.com>
2018-11-20 14:45:26 +0100

    tesg

--- Labo_08/main.cpp
+++ Labo_08/main.cpp
@@ -1,19 +1,18 @@
 /*
  -----------------------------------------------------------------------------------
- Laboratoire : Labo_07_Tir
- Fichier     : Labo_07_TeixeiraCarvalho.cpp
- Auteur(s)   : Stéphane Teixeira Carvalho
- Date        : 13.11.2018
+ Laboratoire : Labo_08_Clavier
+ Fichier     : main.cpp
+ Auteur(s)   : Robin Müller, Stéphane Teixeira Carvalho
+ Date        : 21.11.2018
 
- But         : Afficher une grille de résultats des différents tirs de joueurs lors d'un 
-               concour de tir.
-               Le résultat peut être de 100 au maximum et le joueur perd 1 point 
-               de penalité par cm d'écart du centre.
+ But         : Générer des questions pour tester les réflexes de l'utilisateur.
+               L'utilisateur choisi le nombre de lances entre 1 et 10.
+               Un résultat indiquant le nombre de réponses correcteset le temps effectué
+               est donné en fin de programme
 
- Remarque(s) : Utiliser la fonction srand.
-               Le nombre de joueur sera compris entre 2 et 10.
-               Le nombre de séries sera compris entre 1 et 10.
-               Le nombre de tirs sera compris entre 6 et 12.              
+ Remarque(s) : Utilisation de la fonction ctime pour calculer le temps mis pour résoudre le questionnaire.
+               Le nombre de lances doit être comrpis entre 1 et 10.
+               Utilisation des libraires annexe et reflex.              
 
  Compilateur : MinGW-g++ 6.3.0
  -----------------------------------------------------------------------------------
@@ -42,11 +41,11 @@ int main() {
    
    cout << "ce programme ..." << endl;
    do {
-      //
+      //Saisie de l'utlisateur pour le nombre de lancé
       nbLances = demanderValeur(MSG_NB_LANCES, NB_LANCES_MIN, NB_LANCES_MAX);
-      //
+      //Démarrage du questionnaire
       nbLettresCorrectes = testerReflex(nbLances, LETTRE_MIN, LETTRE_MAX, nbSecondes);
-      //
+      //Affichage du résultat de l'utilisateur
       afficherResultat(nbSecondes, nbLettresCorrectes, nbLances);
    } while(recommencerPartie());
    
